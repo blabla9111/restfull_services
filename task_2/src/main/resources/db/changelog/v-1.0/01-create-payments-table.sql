5aa088820c6b8271b18e636eb7869c5f367020dc
blabla9111 <dinaragindullinaa@gmail.com>
2023-02-21 11:38:29 +0300

    fixed work with docker-compose.yaml and added liquibase

--- task_2/src/main/resources/db/changelog/v-1.0/01-create-payments-table.sql
+++ task_2/src/main/resources/db/changelog/v-1.0/01-create-payments-table.sql
@@ -1,4 +1,6 @@
-CREATE TABLE IF NOT EXISTS payment
+-- CREATE DATABASE payments;
+
+CREATE TABLE IF NOT EXISTS public.payment
 (
     id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1 ),
     name character varying(255) COLLATE pg_catalog."default",
@@ -7,3 +9,9 @@ CREATE TABLE IF NOT EXISTS payment
     value bigint NOT NULL,
     CONSTRAINT payment_pkey PRIMARY KEY (id)
     )
+
+
+    TABLESPACE pg_default;
+--
+-- ALTER TABLE IF EXISTS public.payment
+--     OWNER to root;
